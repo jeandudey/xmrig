51ea3fb69bb3f4e3c509ef2512a43f7bc3229455
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-07-14 20:46:30 +0200

    fixup! Allow using system libfmt

--- cmake/argon2.cmake
+++ cmake/argon2.cmake
@@ -4,21 +4,21 @@
 if (WITH_ARGON2)
     add_definitions(/DXMRIG_ALGO_ARGON2)
 
-    if (WITH_BUNDLED_ARGON2)
-        list(APPEND HEADERS_CRYPTO
-            src/crypto/argon2/Hash.h
-            src/crypto/argon2/Impl.h
-        )
+    list(APPEND HEADERS_CRYPTO
+        src/crypto/argon2/Hash.h
+        src/crypto/argon2/Impl.h
+    )
 
-        list(APPEND SOURCES_CRYPTO
-            src/crypto/argon2/Impl.cpp
-        )
+    list(APPEND SOURCES_CRYPTO
+        src/crypto/argon2/Impl.cpp
+    )
+
+    if (WITH_BUNDLED_ARGON2)
 
         add_subdirectory(src/3rdparty/argon2)
         set(ARGON2_LIBRARY argon2)
     else()
         find_package(Argon2 REQUIRED)
-        set(ARGON2_LIBRARY Argon2)
     endif()
 else()
     remove_definitions(/DXMRIG_ALGO_ARGON2)
