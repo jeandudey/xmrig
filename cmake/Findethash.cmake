24e32fa1b1a329afa1e3694c3368f31c9ef500da
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-07-14 20:53:11 +0200

    fixup! Allow using system libfmt

--- cmake/Findethash.cmake
+++ cmake/Findethash.cmake
@@ -2,4 +2,4 @@ find_path(ETHASH_INCLUDE_DIR NAMES ethash/ethash.h)
 find_library(ETHASH_LIBRARY NAMES ethash ethash.a)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ethash DEFAULT_MSG ARGON2_LIBRARY ARGON2_INCLUDE_DIR)
+find_package_handle_standard_args(ethash DEFAULT_MSG ETHASH_LIBRARY ETHASH_INCLUDE_DIR)
